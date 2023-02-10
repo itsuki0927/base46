71310438e0bb6a48289d439e516cf163cee3dc91
liusenlin <liusenlin.0927@bytedance.com>
2023-02-10 14:03:00 +0800

    chore: add blankline context color

--- lua/base46/integrations/blankline.lua
+++ lua/base46/integrations/blankline.lua
@@ -1,8 +1,13 @@
-local colors = require("base46").get_theme_tb "base_30"
+local colors = require("base46").get_theme_tb("base_30")
 
 return {
   IndentBlanklineChar = { fg = colors.line },
   IndentBlanklineSpaceChar = { fg = colors.line },
-  IndentBlanklineContextChar = { fg = colors.grey },
-  IndentBlanklineContextStart = { bg = colors.one_bg2 },
+  IndentBlanklineContextChar = { fg = colors.grey_fg },
+  IndentBlanklineContextIndent1 = { fg = colors.grey_fg2 },
+  IndentBlanklineContextIndent2 = { fg = colors.red },
+  IndentBlanklineContextIndent3 = { fg = colors.yellow },
+  IndentBlanklineContextIndent4 = { fg = colors.green },
+  IndentBlanklineContextIndent5 = { fg = colors.teal },
+  IndentBlanklineContextIndent6 = { fg = colors.blue },
 }
